662197a12a194901e8580b77ddd6b7ad9c485ef4
Subhasish Nag <subhasish.nag@gmail.com>
2020-04-09 13:34:40 +0530

    Initial Commit

---
+++ src/main/java/com/bpm/jbpm_event_listeners/App.java
@@ -0,0 +1,13 @@
+package com.bpm.jbpm_event_listeners;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
